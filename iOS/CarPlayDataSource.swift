b40e8d0eb771e49b21e56fbb7f3908391bb07c36
Jake Bromberg <jakebromberg@nestlabs.com>
2019-09-18 15:27:40 -0700

    carplay: WIP

---
+++ iOS/CarPlayDataSource.swift
@@ -0,0 +1,22 @@
+//
+//  CarPlayDataSource.swift
+//  WXYC
+//
+//  Created by Jake Bromberg on 9/18/19.
+//  Copyright © 2019 WXYC. All rights reserved.
+//
+
+import Foundation
+import MediaPlayer
+
+//class CarPlayDataSource: NSObject, MPPlayableContentDataSource {
+//  func numberOfChildItems(at indexPath: IndexPath) -> Int {
+//    1
+//  }
+//  
+//  func contentItem(at indexPath: IndexPath) -> MPContentItem? {
+//    <#code#>
+//  }
+//  
+//  
+//}
