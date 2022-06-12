e6568e4ae9871d07713b9132968efb186831d718
Jake Bromberg <jake.bromberg@gmail.com>
2022-06-12 08:03:15 -0700

    Cleaning up Siri stuff

--- Shared/AppDelegate.swift
+++ Shared/AppDelegate.swift
@@ -1,45 +1,41 @@
 import UIKit
 import Combine
 import Core
+import Intents
 import UI
 import MediaPlayer
 import WidgetKit
 
 @UIApplicationMain
+@MainActor
 class AppDelegate: UIResponder, UIApplicationDelegate {
     let cacheCoordinator = CacheCoordinator.WXYCPlaylist
-  
     var nowPlayingObservation: Any?
-    var shouldDonateSiriIntentObservation: Cancellable?
 
     // MARK: UIApplicationDelegate
     
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow()
+        self.window?.makeKeyAndVisible()
+        self.window?.rootViewController = RootPageViewController()
+        
         try! AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-        
-        #if os(iOS)
-            // Make status bar white
-            UINavigationBar.appearance().barStyle = .black
-            self.donateSiriIntentIfNeeded()
-        #endif
-        
-        self.nowPlayingObservation = NowPlayingService.shared.observe { nowPlayingItem in
-            MPNowPlayingInfoCenter.default().update(nowPlayingItem: nowPlayingItem)
-            WidgetCenter.shared.reloadAllTimelines()
-        }
-        
-        WidgetCenter.shared.getCurrentConfigurations { result in
-            guard case let .success(configurations) = result else {
-                return
-            }
+
+        // Make status bar white
+        UINavigationBar.appearance().barStyle = .black
+
+        Task {
+#if os(iOS)
+            await SiriService.shared.donateSiriIntentIfNeeded()
+#endif
             
-            for configuration in configurations {
-                print(configuration)
+            self.nowPlayingObservation = NowPlayingService.shared.observe { nowPlayingItem in
+                MPNowPlayingInfoCenter.default().update(nowPlayingItem: nowPlayingItem)
+                WidgetCenter.shared.reloadAllTimelines()
+                Task { await SiriService.shared.donate(nowPlayingItem: nowPlayingItem) }
             }
-            
-            WidgetCenter.shared.reloadAllTimelines()
         }
         
         return true
@@ -48,59 +44,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         UIApplication.shared.endReceivingRemoteControlEvents()
     }
-}
-
-#if os(iOS)
-import Intents
-
-extension AppDelegate {
-    enum UserSettingsKeys: String {
-        case intentDonated
-    }
     
-    func donateSiriIntentIfNeeded() {
-        Task {
-            guard await self.shouldDonateSiriIntent() else {
-                return
-            }
-            
-            let mediaItem = INMediaItem(
-                identifier: "com.wxyc.ios.intent.play",
-                title: "Play",
-                type: .musicStation,
-                artwork: nil
-            )
-            
-            let intent = INPlayMediaIntent(
-                mediaItems: [mediaItem],
-                mediaContainer: nil,
-                playShuffled: nil,
-                playbackRepeatMode: .none,
-                resumePlayback: false
-            )
-            
-            intent.suggestedInvocationPhrase = "Play WXYC"
-            let interaction = INInteraction(intent: intent, response: nil)
-            
-            try await interaction.donate()
-            await self.cacheCoordinator.set(value: true, for: UserSettingsKeys.intentDonated, lifespan: .distantFuture)
-        }
+#if os(iOS)
+    func application(_ application: UIApplication, handle intent: INIntent) async -> INIntentResponse {
+        return await SiriService.shared.handle(intent: intent)
     }
-
-    func shouldDonateSiriIntent() async -> Bool {
-        do {
-            return try await self.cacheCoordinator.value(for: UserSettingsKeys.intentDonated)
-        } catch {
-            return false
-        }
-    }
-
-    func application(_ application: UIApplication, handle intent: INIntent, completionHandler: @escaping (INIntentResponse) -> Void) {
-        RadioPlayerController.shared.play()
-        
-        let response = INPlayMediaIntentResponse(code: .success, userActivity: nil)
-        completionHandler(response)
-    }
-}
-
 #endif
+}
